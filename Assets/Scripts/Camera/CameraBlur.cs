f6988befa2004573c1c758d3e7c27fef1c9a0de4
grandfrog0 <maksspecial32@gmail.com>
2025-11-10 08:41:51 +0500

    Update CameraBlur.cs

--- Assets/Scripts/Camera/CameraBlur.cs
+++ Assets/Scripts/Camera/CameraBlur.cs
@@ -9,7 +9,10 @@ public class CameraBlur : MonoBehaviour
     private float _startFieldOfView;
     public void SetSpeed(float value)
     {
-        if (_car.GearRatio  0) Camera.main.fieldOfView = _startFieldOfView + value / _car.GearRatio * _multiplier;
+        if (_car.GearRatio > 0)
+        {
+            Camera.main.fieldOfView = _startFieldOfView + value / _car.GearRatio * _multiplier;
+        }
     }
     private void Start()
     {
